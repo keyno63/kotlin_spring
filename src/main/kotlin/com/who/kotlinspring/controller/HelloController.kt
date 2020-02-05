6bc0765443cf3a5d97f2e5250553dcd44652420e
Keyno <kofujiw@gmail.com>
2020-02-05 23:49:00 +0900

    remove import

--- src/main/kotlin/com/who/kotlinspring/controller/HelloController.kt
+++ src/main/kotlin/com/who/kotlinspring/controller/HelloController.kt
@@ -1,6 +1,5 @@
 package com.who.kotlinspring.controller
 
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
