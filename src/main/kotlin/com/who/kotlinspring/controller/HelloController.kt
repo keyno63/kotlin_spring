29a8b5b4302a4ee37966bbe5fdb45733d9cabb3f
Keyno <kofujiw@gmail.com>
2020-02-05 23:58:56 +0900

    add another interface, hello controller.

--- src/main/kotlin/com/who/kotlinspring/controller/HelloController.kt
+++ src/main/kotlin/com/who/kotlinspring/controller/HelloController.kt
@@ -1,13 +1,20 @@
 package com.who.kotlinspring.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/hello")
 class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("")
     fun hello(): String {
         return "hello"
+    }
+
+    @GetMapping("/name")
+    fun string(name: String): String {
+        return "Hello! $name"
     }
 }
