563f3c27ba3816a2f4b4d0b9b154e84cc3d018cb
Keyno <kofujiw@gmail.com>
2020-02-06 23:50:56 +0900

    add post endpoint.

--- src/main/kotlin/com/who/kotlinspring/controller/HelloController.kt
+++ src/main/kotlin/com/who/kotlinspring/controller/HelloController.kt
@@ -1,6 +1,7 @@
 package com.who.kotlinspring.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,5 +17,10 @@ class HelloController {
     @GetMapping("/name")
     fun string(name: String): String {
         return "Hello! $name"
+    }
+
+    @PostMapping("/name")
+    fun stringPost(name: String): String {
+        return "Hello! $name from post."
     }
 }
