10914d950394ca4c8e1c82e2d9cba6f8ec89cb7f
Keyno <kofujiw@gmail.com>
2020-02-10 23:17:48 +0900

    add db cobntroller, sample.

---
+++ src/main/kotlin/com/who/kotlinspring/controller/DBSampleController.kt
@@ -0,0 +1,15 @@
+package com.who.kotlinspring.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/db")
+class DBSampleController {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "test"
+    }
+}
