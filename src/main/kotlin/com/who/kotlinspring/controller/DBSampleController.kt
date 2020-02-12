1c3ae2da94268cc73ad7d7b79db92544dbcc4c40
Keyno <kofujiw@gmail.com>
2020-02-12 23:56:14 +0900

    add db cobntroller, sample, error response.

--- src/main/kotlin/com/who/kotlinspring/controller/DBSampleController.kt
+++ src/main/kotlin/com/who/kotlinspring/controller/DBSampleController.kt
@@ -1,8 +1,11 @@
 package com.who.kotlinspring.controller
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 @RequestMapping("/db")
@@ -16,7 +19,10 @@ class DBSampleController {
     @GetMapping("/get")
     fun get(): String {
 
-        throw NotImplementedError("Not Implememted now.")
-        return ""
+        //throw HttpStatus404Exception("Not Implememted now.")
+        throw HttpStatus404Exception()
     }
 }
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+private class HttpStatus404Exception : RuntimeException()
