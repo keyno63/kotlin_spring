18ee25509b1a3491bb12cfba1617ca8f0148ff09
Keyno <kofujiw@gmail.com>
2020-02-12 23:56:33 +0900

    add db cobntroller, remove vocant line.

--- src/main/kotlin/com/who/kotlinspring/controller/DBSampleController.kt
+++ src/main/kotlin/com/who/kotlinspring/controller/DBSampleController.kt
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @RequestMapping("/db")
 class DBSampleController {
