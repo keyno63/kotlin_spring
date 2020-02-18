a81c5cb8134728501b6ea1135759f921d29010ff
Keyno <kofujiw@gmail.com>
2020-02-18 23:55:33 +0900

    add service bean.

--- src/main/kotlin/com/who/kotlinspring/Service/SampleService.kt
+++ src/main/kotlin/com/who/kotlinspring/Service/SampleService.kt
@@ -1,6 +1,8 @@
 package com.who.kotlinspring.Service
 
 import com.who.kotlinspring.repository.SampleRepository
+import org.springframework.stereotype.Service
 
+@Service
 class SampleService(sr: SampleRepository) {
 }
