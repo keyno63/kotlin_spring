ddc1df03c72f5cf1290e611e0ab3b223d27aa711
Keyno <kofujiw@gmail.com>
2020-03-06 23:40:28 +0900

    Service

--- src/main/kotlin/com/who/kotlinspring/Service/SampleService.kt
+++ src/main/kotlin/com/who/kotlinspring/Service/SampleService.kt
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class SampleService(val sr: SampleRepository): SampleServiceBase {
+class SampleServiceImpl(val sr: SampleRepository): SampleServiceBase {
 
     fun get(x: String): String {
         return sr.get(x)
