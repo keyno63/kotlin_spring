657af5530983129064270b943f2f19e4ce35604c
Keyno <kofujiw@gmail.com>
2020-03-14 23:26:07 +0900

    Service

--- src/main/kotlin/com/who/kotlinspring/Service/SampleServiceImpl.kt
+++ src/main/kotlin/com/who/kotlinspring/Service/SampleServiceImpl.kt
@@ -1,7 +1,6 @@
 package com.who.kotlinspring.Service
 
 import com.who.kotlinspring.repository.SampleRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
