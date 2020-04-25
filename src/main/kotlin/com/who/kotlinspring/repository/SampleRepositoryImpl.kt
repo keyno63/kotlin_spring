e03127fa0e09ee6e8a429deccf0db8a3428394a8
Keyno <kofujiw@gmail.com>
2020-04-26 00:39:08 +0900

    add override

--- src/main/kotlin/com/who/kotlinspring/repository/SampleRepositoryImpl.kt
+++ src/main/kotlin/com/who/kotlinspring/repository/SampleRepositoryImpl.kt
@@ -1,14 +1,14 @@
-package com.who.kotlinspring.repository;
+package com.who.kotlinspring.repository
 
 import org.springframework.stereotype.Repository
 
 @Repository
 class SampleRepositoryImpl: SampleRepository {
-    fun get(x: String): String {
+    override fun get(x: String): String {
         return ""
     }
 
-    fun post(x: String): String {
+    override fun post(x: String): String {
         return ""
     }
 
